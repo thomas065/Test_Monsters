cfcbc310746e754752740236a4235b2a9da3e6f0
Thomas Bell <thomasjbell065@gmail.com>
2023-10-29 15:20:10 -0400

    Add project files.

---
+++ Controllers/MonsterController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test_Monsters.Models;
+
+namespace Test_Monsters.Controllers
+{
+	public class MonsterController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			MonsterData hero = new MonsterData();
+
+			hero.Hitpoints = 0;
+			hero.Mana = 0;
+			hero.Stamina = 0;
+			hero.Strength = 0;
+
+			return View(hero);
+		}
+
+		[HttpPost]
+		public IActionResult Index(MonsterData hero)
+		{
+			hero.Name = $"Name: {hero.Name}";
+			hero.Hitpoints = 35;
+			hero.Stamina = 100;
+			hero.Strength = 12;
+			hero.Mana = 0;
+
+			return View(hero);
+		}
+	}
+}
---
+++ Models/MonsterData.cs
@@ -0,0 +1,18 @@
+﻿namespace Test_Monsters.Models
+{
+	public class MonsterData
+	{
+        public string? Name { get; set; }
+
+        public int Hitpoints { get; set; }
+
+        public int Mana { get; set; }
+
+        public int Stamina { get; set; }
+
+        public int Strength { get; set; }
+
+        public string? Bio { get; set; }
+
+    }
+}
